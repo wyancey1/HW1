a0d478c81c5b4648d6342ac529ae65422a9023f3
William Yancey <william_yancey1@baylor.edu>
2023-08-31 14:48:00 -0500

    read V2

--- read_philosophers.py
+++ read_philosophers.py
@@ -1,7 +1,13 @@
 def main():
     infile = open('philosophers.txt','r')
     
-    info = infile.read()
+    line1 = infile.readline().rstrip('\n')
+    line2 = infile.readline().rstrip('\n')
+    line3 = infile.readline().rstrip('\n')
 
-    print(info)
+    print(line1)
+    print(line2)
+    print(line3)
+
+    #print(info)
 main()
