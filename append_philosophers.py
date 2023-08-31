a3ae78822580774ea3fcd6d5ac8be476b75b7910
William Yancey <william_yancey1@baylor.edu>
2023-08-31 14:51:01 -0500

    append V1

---
+++ append_philosophers.py
@@ -0,0 +1,10 @@
+philosophers = ['John Locke', 'David Hume', 'Edmund Burke']
+
+def main():
+    outfile = open('philosophers.txt','a')
+
+    outfile.write('William Yancey')
+
+    outfile.close()
+
+main()
